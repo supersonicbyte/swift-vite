1cba66d7b55bcf47183055869c34bcf0547b4f2e
Ucanbarlic <mirza.ucanbarlic@gmail.com>
2024-10-16 17:10:25 +0200

    Project init

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "swift-vite",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "swift-vite",
+            targets: ["swift-vite"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "swift-vite"),
+        .testTarget(
+            name: "swift-viteTests",
+            dependencies: ["swift-vite"]
+        ),
+        .executableTarget(
+            name: "Development",
+            dependencies: [
+                .target(name: "swift-vite")
+            ]
+        )
+    ]
+)
---
+++ Sources/Development/main.swift
@@ -0,0 +1,8 @@
+//
+//  main.swift
+//  swift-vite
+//
+//  Created by Ucanbarlic on 16. 10. 2024..
+//
+
+print("Hello world!")
