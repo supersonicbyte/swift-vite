86b791724f4c317f26989d2fc48ee0f832f5f346
Grant J. Butler <grantjbutler@users.noreply.github.com>
2024-10-27 20:03:33 -0400

    Add tests for rendering entry points.

--- Sources/SwiftVite/ViteManifest.swift
+++ Sources/SwiftVite/ViteManifest.swift
@@ -5,7 +5,7 @@ public typealias ViteManifest = [String: ViteManifestEntry]
 public struct ViteManifestEntry: Decodable, Sendable {
     public let file: String
     public let name: String?
-    public let src: String
+    public let src: String?
     public let isEntry: Bool
     public let imports: [String]
     public let dynamicImports: [String]
@@ -25,7 +25,7 @@ public struct ViteManifestEntry: Decodable, Sendable {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         self.file = try container.decode(String.self, forKey: .file)
         self.name = try container.decodeIfPresent(String.self, forKey: .name)
-        self.src = try container.decode(String.self, forKey: .src)
+        self.src = try container.decodeIfPresent(String.self, forKey: .src)
         self.isEntry = try container.decodeIfPresent(Bool.self, forKey: .isEntry) ?? false
         self.imports = try container.decodeIfPresent([String].self, forKey: .imports) ?? []
         self.dynamicImports = try container.decodeIfPresent([String].self, forKey: .dynamicImports) ?? []
---
+++ Tests/SwiftViteTests/ViteTests.swift
@@ -0,0 +1,104 @@
+import Foundation
+import Testing
+@testable import SwiftVite
+
+@Suite
+struct ViteTests {
+    @Test func renders_tags_for_production() throws {
+        try withManifest("""
+        {
+            "_shared-Csw0LVKT.js": {
+                "file": "assets/shared-ChJ_j-JJ.css",
+                "src": "_shared-Csw0LVKT.js"
+            },
+            "_shared-B7PI925R.js": {
+                "file": "assets/shared-B7PI925R.js",
+                "name": "shared",
+                "css": ["assets/shared-ChJ_j-JJ.css"]
+            },
+            "baz.js": {
+                "file": "assets/baz-B2H3sXNv.js",
+                "name": "baz",
+                "src": "baz.js",
+                "isDynamicEntry": true
+            },
+            "views/bar.js": {
+                "file": "assets/bar-gkvgaI9m.js",
+                "name": "bar",
+                "src": "views/bar.js",
+                "isEntry": true,
+                "imports": ["_shared-B7PI925R.js"],
+                "dynamicImports": ["baz.js"]
+            },
+            "views/foo.js": {
+                "file": "assets/foo-BRBmoGS9.js",
+                "name": "foo",
+                "src": "views/foo.js",
+                "isEntry": true,
+                "imports": ["_shared-B7PI925R.js"],
+                "css": ["assets/foo-5UjPuW-k.css"]
+            }
+        }
+        """) { manifest in
+            let vite = Vite(manifest: manifest, buildDirectory: "build", environment: .production)
+            let renderedTags = try vite.tags(forEntryPoint: "views/foo.js")
+            
+            #expect(renderedTags == """
+            <link rel="stylesheet" href="build/assets/foo-5UjPuW-k.css">
+            <link rel="stylesheet" href="build/assets/shared-ChJ_j-JJ.css">
+            <script type="module" src="build/assets/foo-BRBmoGS9.js"></script>
+            """)
+        }
+    }
+    
+    @Test func renders_tags_for_development() throws {
+        try withManifest("""
+        {
+            "_shared-Csw0LVKT.js": {
+                "file": "assets/shared-ChJ_j-JJ.css",
+                "src": "_shared-Csw0LVKT.js"
+            },
+            "_shared-B7PI925R.js": {
+                "file": "assets/shared-B7PI925R.js",
+                "name": "shared",
+                "css": ["assets/shared-ChJ_j-JJ.css"]
+            },
+            "baz.js": {
+                "file": "assets/baz-B2H3sXNv.js",
+                "name": "baz",
+                "src": "baz.js",
+                "isDynamicEntry": true
+            },
+            "views/bar.js": {
+                "file": "assets/bar-gkvgaI9m.js",
+                "name": "bar",
+                "src": "views/bar.js",
+                "isEntry": true,
+                "imports": ["_shared-B7PI925R.js"],
+                "dynamicImports": ["baz.js"]
+            },
+            "views/foo.js": {
+                "file": "assets/foo-BRBmoGS9.js",
+                "name": "foo",
+                "src": "views/foo.js",
+                "isEntry": true,
+                "imports": ["_shared-B7PI925R.js"],
+                "css": ["assets/foo-5UjPuW-k.css"]
+            }
+        }
+        """) { manifest in
+            let vite = Vite(manifest: manifest, buildDirectory: "build", environment: .development)
+            let renderedTags = try vite.tags(forEntryPoint: "views/foo.js")
+            
+            #expect(renderedTags == """
+            <script type="module" src="http://localhost:5173/@vite/client"></script>
+            <script type="module" src="http://localhost:5173/views/foo.js"></script>
+            """)
+        }
+    }
+}
+
+func withManifest(_ manifestString: String, _ handler: (ViteManifest) throws -> Void) throws {
+    let manifest = try JSONDecoder().decode(ViteManifest.self, from: Data(manifestString.utf8))
+    try handler(manifest)
+}
